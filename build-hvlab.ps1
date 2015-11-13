aeadffa20a3af1bb54a2e31b8d75f26d8002c7fb
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-13 18:05:39 +0100

    initial commit for just running -update

---
+++ build-hvlab.ps1
@@ -0,0 +1,439 @@
+﻿<#
+.Synopsis
+   labbuildr-flex allows you to create Virtual Machines with VMware Workstation from Predefined Scenarios.
+   Scenarios include Exchange 2013, SQL, Hyper-V, SCVMM .. .
+   labbuildr-flex runs on EMC VLAB Flex Environment
+.DESCRIPTION
+   labbuildr is a Self Installing Lab tool for Building VMware Virtual Machines on VMware Workstation
+      
+      Copyright 2015 Karsten Bott
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+.LINK
+   https://github.com/bottkars/labbuildr-flex
+#>
+[CmdletBinding(DefaultParametersetName = "version",
+    SupportsShouldProcess=$true,
+    ConfirmImpact="Medium")]
+	[OutputType([psobject])]
+param (
+    <#run build-lab update    #>
+	[Parameter(ParameterSetName = "update",Mandatory = $false, HelpMessage = "this will update labbuildr from latest git commit")][switch]$Update,
+    <#
+    run build-lab update    #>
+	[Parameter(ParameterSetName = "update",Mandatory = $false, HelpMessage = "select a branch to update from")][ValidateSet('master','testing','develop')]$branch  = "develop",
+    [Parameter(ParameterSetName = "update",Mandatory = $false, HelpMessage = "this will force update labbuildr")]
+    [switch]$force,
+    <#Exchange 2016   #>
+	[Parameter(ParameterSetName = "E16",Mandatory = $true)][switch][alias('ex16')]$Exchange2016,
+
+    <#
+    Determines Exchange CU Version to be Installed
+    Valid Versions are:
+    'Final
+    Default is latest
+    CU Location is [Driveletter]:\sources\e2016[cuver], e.g. c:\sources\e2016Preview1
+    #>
+	[Parameter(ParameterSetName = "E16", Mandatory = $false)]
+    [ValidateSet('final')]$e16_cu,
+    
+    [String]$Sourcedir
+
+)
+$Builddir = $PSScriptRoot
+$Sourcedir = "C:\Sources"
+if (!(Test-Path $Sourcedir))
+    {
+    New-Item -ItemType Directory -Path $Sourcedir | out-null
+    }
+if (!(get-smbshare -name "Sources"))
+    {
+    new-smbshare -name "Sources" -path $Sourcedir
+    }
+##
+$Latest_e16_cu = "final"
+
+try
+    {
+    [datetime]$Latest_labbuildr_hyperv_git = Get-Content  ($Builddir + "\labbuildr-hyperv-$branch.gitver") -ErrorAction Stop
+    }
+    catch
+    {
+    [datetime]$Latest_labbuildr_hyperv_git = "07/11/2015"
+    }
+try
+    {
+    [datetime]$Latest_labbuildr_scripts_git = Get-Content  ($Builddir + "\labbuildr-scripts-$branch.gitver") -ErrorAction Stop
+    }
+    catch
+    {
+    [datetime]$Latest_labbuildr_scripts_git = "07/11/2015"
+    }
+
+
+
+####
+function update-fromGit
+{
+
+
+	param (
+            [string]$Repo,
+            [string]$RepoLocation,
+            [string]$branch,
+            [string]$latest_local_Git,
+            [string]$Destination,
+            [switch]$delete
+            )
+
+
+        Write-Verbose "Using update-fromgit function for $repo"
+        $Uri = "https://api.github.com/repos/$RepoLocation/$repo/commits/$branch"
+        $Zip = ("https://github.com/$RepoLocation/$repo/archive/$branch.zip").ToLower()
+        $request = Invoke-WebRequest -UseBasicParsing -Uri $Uri -Method Head
+        [datetime]$latest_OnGit = $request.Headers.'Last-Modified'
+                Write-Verbose "We have $repo version $latest_local_Git, $latest_OnGit is online !"
+                if ($latest_local_Git -lt $latest_OnGit -or $force.IsPresent )
+                    {
+                    $Updatepath = "$Builddir\Update"
+					if (!(Get-Item -Path $Updatepath -ErrorAction SilentlyContinue))
+					        {
+						    $newDir = New-Item -ItemType Directory -Path "$Updatepath"
+                            }
+                    Write-Output "We found a newer Version for $repo on Git Dated $($request.Headers.'Last-Modified')"
+                    if ($delete.IsPresent)
+                        {
+                        Write-Verbose "Cleaning $Destination"
+                        Remove-Item -Path $Destination -Recurse -ErrorAction SilentlyContinue
+                        }
+                    Get-LABHttpFile -SourceURL $Zip -TarGetFile "$Builddir\update\$repo-$branch.zip" -ignoresize
+                    Expand-LABZip -zipfilename "$Builddir\update\$repo-$branch.zip" -destination $Destination -Folder $repo-$branch
+                    $Isnew = $true
+                    $request.Headers.'Last-Modified' | Set-Content ($Builddir+"\$repo-$branch.gitver") 
+                    }
+                else 
+                    {
+                    Write-Warning "No update required for labbuildr-hyperv, already newest version "
+                    }
+
+}
+#####
+function Get-LABHttpFile
+ {
+    [CmdletBinding(DefaultParametersetName = "1",
+    HelpUri = "https://github.com/bottkars/LABbuildr/wiki/LABtools#GET-LABHttpFile")]
+	param (
+	[Parameter(ParameterSetName = "1", Mandatory = $true,Position = 0)]$SourceURL,
+    [Parameter(ParameterSetName = "1", Mandatory = $false)]$TarGetFile,
+    [Parameter(ParameterSetName = "1", Mandatory = $false)][switch]$ignoresize
+    )
+
+
+begin
+{}
+process
+{
+if (!$TarGetFile)
+    {
+    $TarGetFile = Split-Path -Leaf $SourceURL
+    }
+try
+                    {
+                    $Request = Invoke-WebRequest $SourceURL -UseBasicParsing -Method Head
+                    }
+                catch [Exception] 
+                    {
+                    Write-Warning "Could not downlod $SourceURL"
+                    Write-Warning $_.Exception
+                    break
+                    }
+                
+                $Length = $request.Headers.'content-length'
+                try
+                    {
+                    # $Size = "{0:N2}" -f ($Length/1GB)
+                    # Write-Warning "
+                    # Trying to download $SourceURL 
+                    # The File size is $($size)GB, this might take a while....
+                    # Please do not interrupt the download"
+                    Invoke-WebRequest $SourceURL -OutFile $TarGetFile
+                    }
+                catch [Exception] 
+                    {
+                    Write-Warning "Could not downlod $SourceURL. please download manually"
+                    Write-Warning $_.Exception
+                    break
+                    }
+                if ( (Get-ChildItem  $TarGetFile).length -ne $Length -and !$ignoresize)
+                    {
+                    Write-Warning "File size does not match"
+                    Remove-Item $TarGetFile -Force
+                    break
+                    }                       
+
+
+}
+end
+{}
+}                 
+###
+function Expand-LABZip
+{
+ [CmdletBinding(DefaultParameterSetName='Parameter Set 1',
+    HelpUri = "https://github.com/bottkars/LABbuildr/wiki/LABtools#Expand-LABZip")]
+	param (
+        [string]$zipfilename,
+        [string] $destination,
+        [String]$Folder)
+	$copyFlag = 16 # overwrite = yes
+	$Origin = $MyInvocation.MyCommand
+	if (test-path($zipfilename))
+	{
+    If ($Folder)
+        {
+        $zipfilename = Join-Path $zipfilename $Folder
+        }
+    		
+        Write-Verbose "extracting $zipfilename to $destination"
+        if (!(test-path  $destination))
+            {
+            New-Item -ItemType Directory -Force -Path $destination | Out-Null
+            }
+        $shellApplication = New-object -com shell.application
+		$zipPackage = $shellApplication.NameSpace($zipfilename)
+		$destinationFolder = $shellApplication.NameSpace("$destination")
+		$destinationFolder.CopyHere($zipPackage.Items(), $copyFlag)
+	}
+}
+
+##
+function Extract-Zip
+{
+	param ([string]$zipfilename, [string] $destination)
+	$copyFlag = 16 # overwrite = yes
+	$Origin = $MyInvocation.MyCommand
+	if (test-path($zipfilename))
+	{		
+        if (!(Test-Path $destination))
+            {New-Item -ItemType Directory -Path $destination -Force | Out-Null }
+        Write-Verbose "extracting $zipfilename"
+        $shellApplication = new-object -com shell.application
+		$zipPackage = $shellApplication.NameSpace($zipfilename)
+		$destinationFolder = $shellApplication.NameSpace($destination)
+		$destinationFolder.CopyHere($zipPackage.Items(), $copyFlag)
+	}
+}
+####
+
+function get-prereq
+{ 
+param ([string]$DownLoadUrl,
+        [string]$destination )
+$ReturnCode = $True
+if (!(Test-Path $Destination))
+    {
+        Try 
+        {
+        if (!(Test-Path (Split-Path $destination)))
+            {
+            New-Item -ItemType Directory  -Path (Split-Path $destination) -Force
+            }
+        Write-verbose "Starting Download of $DownLoadUrl"
+        Start-BitsTransfer -Source $DownLoadUrl -Destination $destination -DisplayName "Getting $destination" -Priority Foreground -Description "From $DownLoadUrl..." -ErrorVariable err 
+                If ($err) {Throw ""} 
+
+        } 
+        Catch 
+        { 
+            $ReturnCode = $False 
+            Write-Warning " - An error occurred downloading `'$FileName`'" 
+            Write-Error $_ 
+        }
+    }
+    else
+    {
+    write-Warning "No download needed, file exists" 
+    }
+    return $ReturnCode 
+}
+####
+
+switch ($PsCmdlet.ParameterSetName)
+{
+    "update" 
+        {
+
+
+        $Repo = "labbuildr-hyperv"
+        $RepoLocation = "bottkars"
+        $Latest_local_git = $Latest_labbuildr_hyperv_git
+        $Destination = "$Builddir"
+        update-fromGit -Repo $Repo -RepoLocation $RepoLocation -branch $branch -latest_local_Git $Latest_local_git -Destination $Destination
+        if (Test-Path "$Builddir\deletefiles.txt")
+		    {
+			$deletefiles = get-content "$Builddir\deletefiles.txt"
+			foreach ($deletefile in $deletefiles)
+			    {
+				if (Get-Item $Builddir\$deletefile -ErrorAction SilentlyContinue)
+				    {
+					Remove-Item -Path $Builddir\$deletefile -Recurse -ErrorAction SilentlyContinue
+					status "deleted $deletefile"
+					write-log "deleted $deletefile"
+					}
+			    }
+            }
+        else 
+            {
+            Write-Host "No Deletions required"
+            }
+        $Repo = "labbuildr-scripts"
+        $RepoLocation = "bottkars"
+        $Latest_local_git = $Latest_labbuildr_scripts_git
+        $Destination = "$Builddir\Scripts"
+        update-fromGit -Repo $Repo -RepoLocation $RepoLocation -branch $branch -latest_local_Git $Latest_local_git -Destination $Destination -delete
+
+            return
+    }# end Updatefromgit
+}
+
+if ($Exchange2016.IsPresent)
+{
+    if (!$e16_cu)
+        {
+        $e16_cu = $Latest_e16_cu
+        }
+    $EX_Version = "E2016"
+    $Scenarioname = "Exchange"
+    $Prereqdir = "Attachments"
+    $attachments = (
+    "http://www.cisco.com/c/dam/en/us/solutions/collateral/data-center-virtualization/unified-computing/fle_vmware.pdf"
+   # "http://www.emc.com/collateral/white-papers/h12234-emc-integration-for-microsoft-private-cloud-wp.pdf",
+   # "http://www.emc.com/collateral/software/data-sheet/h2257-networker-ds.pdf",
+   # "http://www.emc.com/collateral/software/data-sheet/h2479-networker-app-modules-ds.pdf",
+   # "http://www.emc.com/collateral/software/data-sheet/h4525-networker-ms-apps-ds.pdf",
+   # "http://www.emc.com/collateral/handouts/h14152-cloudboost-handout.pdf",
+   # "http://www.emc.com/collateral/software/data-sheet/h2257-networker-ds.pdf",
+   # "http://www.emc.com/collateral/software/data-sheet/h3979-networker-dedupe-ds.pdf"
+    )
+    
+    if (!(Test-Path $Sourcedir\$Prereqdir)){New-Item -ItemType Directory -Path $Sourcedir\$Prereqdir | Out-Null }
+     foreach ($URL in $attachments)
+        {
+        $FileName = Split-Path -Leaf -Path $Url
+        if (!(test-path  $Sourcedir\$Prereqdir\$FileName))
+            {
+            Write-Verbose "$FileName not found, trying Download"
+            if (!(get-prereq -DownLoadUrl $URL -destination $Sourcedir\$Prereqdir\$FileName))
+                { write-warning "Error Downloading file $Url, Please check connectivity"
+                  Write-Warning "Creating Dummy File"
+                  New-Item -ItemType file -Path "$Sourcedir\$Prereqdir\$FileName"
+                }
+            }
+
+        
+        }
+    
+    $Prereqdir = $EX_Version+"prereq"
+
+    Write-Verbose "We are now going to Test $EX_Version Prereqs"
+    $DownloadUrls = (
+		        "http://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe",
+#                "http://download.microsoft.com/download/A/A/3/AA345161-18B8-45AE-8DC8-DA6387264CB9/filterpack2010sp1-kb2460041-x64-fullfile-en-us.exe",
+#                "http://download.microsoft.com/download/0/A/2/0A28BBFA-CBFA-4C03-A739-30CCA5E21659/FilterPack64bit.exe",
+                "http://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe"
+ #               "http://download.microsoft.com/download/6/2/D/62DFA722-A628-4CF7-A789-D93E17653111/ExchangeMapiCdo.EXE"
+                
+                ) 
+    if (Test-Path -Path "$Sourcedir\$Prereqdir")
+        {
+        Write-Verbose "$EX_Version Sourcedir Found"
+        }
+        else
+        {
+        Write-Verbose "Creating Sourcedir for $EX_Version Prereqs"
+        New-Item -ItemType Directory -Path $Sourcedir\$Prereqdir | Out-Null
+        }
+
+
+    foreach ($URL in $DownloadUrls)
+        {
+        $FileName = Split-Path -Leaf -Path $Url
+        if (!(test-path  $Sourcedir\$Prereqdir\$FileName))
+            {
+            Write-Verbose "$FileName not found, trying Download"
+            if (!(get-prereq -DownLoadUrl $URL -destination $Sourcedir\$Prereqdir\$FileName))
+                { write-warning "Error Downloading file $Url, Please check connectivity"
+                exit
+                }
+            }
+
+        
+        }
+
+    write-verbose "Testing $Sourcedir/$EX_Version$e16_cu/setup.exe"
+    if (Test-Path "$Sourcedir/$EX_Version$e16_cu/setup.exe")
+        {
+        Write-Verbose "E16 $e16_cu Found"
+        }
+        else
+        {
+        Write-Warning "We need to Extract $EX_Version $e16_cu, this may take a while"
+        # New-Item -ItemType Directory -Path $Sourcedir\$EX_Version$ex_cu | Out-Null
+        # }
+        Switch ($e16_cu)
+
+            {
+                "final"
+                {
+                $URL = "http://download.microsoft.com/download/3/9/B/39B8DDA8-509C-4B9E-BCE9-4CD8CDC9A7DA/Exchange2016-x64.exe"
+                }
+
+            }
+
+        $FileName = Split-Path -Leaf -Path $Url
+        if (!(test-path  $Sourcedir\$FileName))
+            {
+            "We need to Download $EX_Version $e16_cu from $url, this may take a while"
+            if (!(get-prereq -DownLoadUrl $URL -destination $Sourcedir\$FileName))
+                { write-warning "Error Downloading file $Url, Please check connectivity"
+                exit
+            }
+        }
+        Write-Verbose "Extracting $FileName"
+        Start-Process -FilePath "$Sourcedir\$FileName" -ArgumentList "/extract:$Sourcedir\$EX_Version$e16_cu /passive" -Wait
+            
+    } #end else
+    if (!(Test-Path $Sourcedir\attachments))
+        {
+         Write-Warning "attachments Directory not found. Please Create $Sourcedir\attachments and copy some Documents for Mail and Public Folder Deployment"
+            }
+        else
+            {
+            Write-Verbose "Found attachments"
+            }
+	    if ($DAG.IsPresent)
+	        {
+		    Write-Host -ForegroundColor Yellow "We will form a $EXNodes-Node DAG"
+	        }
+
+}
+
+
+
+switch ($PsCmdlet.ParameterSetName)
+    {
+        "E16"
+        {
+        $EXnode1 = "HV01"
+        }
+    }
---
+++ clone-node.ps1
@@ -0,0 +1,47 @@
+﻿
+[CmdletBinding()]
+Param(
+[Parameter(Mandatory=$false)][string]$Builddir = $PSScriptRoot,
+[Parameter(Mandatory=$true)][string]$MasterVHD,
+[Parameter(Mandatory=$false)][string]$Domainname,
+[Parameter(Mandatory=$true)][string]$Nodename,
+# [Parameter(Mandatory=$false)][string]$CloneVMPath = "$Builddir\$Nodename\$Nodename.vmx",
+[Parameter(Mandatory=$false)][string]$vmnet ="vmnet2",
+[Parameter(Mandatory=$false)][switch]$Isilon,
+[Parameter(Mandatory=$false)][string]$scenarioname = "Default",
+[Parameter(Mandatory=$false)][int]$Scenario = 1,
+[Parameter(Mandatory=$false)][int]$ActivationPreference = 1,
+[Parameter(Mandatory=$false)][switch]$AddDisks,
+[Parameter(Mandatory=$false)][uint64]$Disksize = 200GB,
+[Parameter(Mandatory=$false)][ValidateRange(1, 6)][int]$Disks = 1,
+[Parameter(Mandatory=$false)][ValidateSet('XS','S','M','L','XL','TXL','XXL','XXXL')]$Size = "M",
+[switch]$Exchange,
+[switch]$HyperV,
+[switch]$NW,
+[switch]$Bridge,
+[switch]$Gateway,
+[switch]$sql,
+$Sourcedir
+)
+
+##size eval
+$vlanid = 2
+
+###disks eval
+
+
+
+
+New-VHD –Path “$Builddir\$Nodename\$Nodename.vhdx” –ParentPath “$MasterVHD” 
+$CloneVM = New-VM -Name $Nodename -Path "$Builddir\$Nodename" -Memory 512MB  -VHDPath "$Builddir\$Nodename\$Nodename.vhdx” -SwitchName VMnet -Generation 2
+$CloneVM | Set-VMMemory -DynamicMemoryEnabled $true -MinimumBytes 128MB -StartupBytes 512MB -MaximumBytes 2GB -Priority 80 -Buffer 25
+$CloneVM | Get-VMHardDiskDrive | Set-VMHardDiskDrive -MaximumIOPS 2000
+$CloneVM | Set-VM –AutomaticStartAction Start
+if ($vlanid)
+    {
+    $CloneVM | Get-VMNetworkAdapter | Set-VMNetworkAdapterVlan -Access -VlanId $vlanid
+    }
+$CloneVM | start-vm
+$CloneVM | Get-VM 
+
+# return ok
