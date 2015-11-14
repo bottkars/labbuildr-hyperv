ad5d7ffaf972952577e9b4b16627c794634f9af3
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-14 10:17:52 +0100

    sysprep1

--- clone-node.ps1
+++ clone-node.ps1
@@ -5,7 +5,7 @@ Param(
 [Parameter(Mandatory=$true)][string]$MasterVHD,
 [Parameter(Mandatory=$false)][string]$Domainname,
 [Parameter(Mandatory=$true)][string]$Nodename,
-# [Parameter(Mandatory=$false)][string]$CloneVMPath = "$Builddir\$Nodename\$Nodename.vmx",
+[Parameter(Mandatory=$false)][string]$CloneVMPath = "$Builddir\$Nodename\",
 [Parameter(Mandatory=$false)][string]$vmnet ="vmnet2",
 [Parameter(Mandatory=$false)][switch]$Isilon,
 [Parameter(Mandatory=$false)][string]$scenarioname = "Default",
@@ -15,6 +15,7 @@ Param(
 [Parameter(Mandatory=$false)][uint64]$Disksize = 200GB,
 [Parameter(Mandatory=$false)][ValidateRange(1, 6)][int]$Disks = 1,
 [Parameter(Mandatory=$false)][ValidateSet('XS','S','M','L','XL','TXL','XXL','XXXL')]$Size = "M",
+$vlanid = 2,
 [switch]$Exchange,
 [switch]$HyperV,
 [switch]$NW,
@@ -25,7 +26,6 @@ $Sourcedir
 )
 
 ##size eval
-$vlanid = 2
 
 ###disks eval
 
@@ -33,8 +33,10 @@ $vlanid = 2
 
 
 New-VHD –Path “$Builddir\$Nodename\$Nodename.vhdx” –ParentPath “$MasterVHD” 
-$CloneVM = New-VM -Name $Nodename -Path "$Builddir\$Nodename" -Memory 512MB  -VHDPath "$Builddir\$Nodename\$Nodename.vhdx” -SwitchName VMnet -Generation 2
-$CloneVM | Set-VMMemory -DynamicMemoryEnabled $true -MinimumBytes 128MB -StartupBytes 512MB -MaximumBytes 2GB -Priority 80 -Buffer 25
+$CloneVM = New-VM -Name $Nodename -Path "$Builddir\$Nodename" -Memory 512MB  -VHDPath "$Builddir\$Nodename\$Nodename.vhdx” -SwitchName $VMnet -Generation 2
+$CloneVM | Set-VMMemory -DynamicMemoryEnabled $true -MinimumBytes 128MB -StartupBytes 1024MB -MaximumBytes 2GB -Priority 80 -Buffer 25
+$CloneVM | Add-VMDvdDrive -Path "$Builddir\$Nodename\build.iso"
+$CloneVM | Set-VMProcessor -Count 2
 $CloneVM | Get-VMHardDiskDrive | Set-VMHardDiskDrive -MaximumIOPS 2000
 $CloneVM | Set-VM –AutomaticStartAction Start
 if ($vlanid)
