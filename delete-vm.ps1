173fd6c44ac4578303f14c135cf64da94d2f4bd6
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-20 14:19:51 +0100

    delte vm added

---
+++ delete-vm.ps1
@@ -0,0 +1,14 @@
+﻿param (
+$vmname)
+try
+    {
+    get-vm $vmname -ErrorAction Stop 
+    }
+catch
+    {
+    write-host "VM Not Found"
+    exit
+    }  
+get-vm $vmname -ErrorAction Stop | Stop-VM -Force -TurnOff
+get-vm $vmname | Remove-VM -Force
+Remove-Item ".\$vmname" -Force -Recurse
